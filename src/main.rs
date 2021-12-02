f8662d32c81a2f9374e9158a33e5fde13ee877bd
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-12-01 22:27:39 -0800

    setup system for organizing days and add some libraries ill probably want

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use advent_of_code_2021::problems::{problem_one, problem_one_part_two};
+use structopt::StructOpt;
 
 fn main() {
-    problem_one();
-    problem_one_part_two();
+    #[derive(StructOpt)]
+    #[structopt(about = "My solutions to AoC 2021.")]
+    struct Opt {
+        #[structopt(long)]
+        one: bool,
+    }
+
+    let opt = Opt::from_args();
+
+    if opt.one {
+        problem_one();
+        problem_one_part_two();
+    }
 }
