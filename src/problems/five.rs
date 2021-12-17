0c1d61eb0f0a79f546d5312fbc4f3aa60aa8ffb1
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-12-17 00:04:28 -0800

    parsing code for day five done

---
+++ src/problems/five.rs
@@ -0,0 +1,89 @@
+use crate::problems::ProblemSet;
+
+
+use std::str::FromStr;
+
+
+pub const PROBLEM_SET: ProblemSet = ProblemSet { part_a, part_b };
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl FromStr for Point {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if parts.len() != 2 {
+            return Err(());
+        }
+
+        let x: i32 = parts[0].trim().parse().map_err(|_| ())?;
+        let y: i32 = parts[1].trim().parse().map_err(|_| ())?;
+
+        Ok(Point::new(x, y))
+    }
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug)]
+struct LineSegment {
+    a: Point,
+    b: Point,
+}
+
+impl LineSegment {
+    fn new(a: Point, b: Point) -> Self {
+        Self { a, b }
+    }
+}
+
+impl FromStr for LineSegment {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split("->").collect();
+        if parts.len() != 2 {
+            return Err(());
+        }
+        let a: Point = parts[0].parse()?;
+        let b: Point = parts[1].parse()?;
+
+        Ok(LineSegment::new(a, b))
+    }
+}
+
+#[must_use] pub fn part_a(problem_text: &str) -> String {
+    let segments: Vec<LineSegment> = problem_text
+        .lines()
+        .filter_map(|line| {
+            let seg: LineSegment = line.parse().ok()?;
+            Some(seg)
+        })
+        .collect();
+
+    format!("{:?}", segments)
+}
+
+#[must_use] pub fn part_b(_problem_text: &str) -> String {
+    "hello_world".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    const PROBLEM_TEXT: &str = include_str!("inputs/problem_5.txt");
+
+    #[test]
+    fn part_a() {
+        assert_eq!(super::part_a(PROBLEM_TEXT), "1529");
+    }
+}
